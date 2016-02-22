e81fe8b3d35c7d6be04c56f3fae9fa32de923b2b
David Costa <dav.costa90@gmail.com>
2016-02-22 12:19:13 +0100

    ResourceDirectory as a Singleton

--- src/main/java/it/dc/bridge/rd/ResourceDirectory.java
+++ src/main/java/it/dc/bridge/rd/ResourceDirectory.java
@@ -13,14 +13,15 @@ import org.eclipse.californium.core.network.config.NetworkConfig;
  * the functions required to register, maintain and remove the
  * CoAP resources. It does not implement resource lookup, as
  * described in draft-ietf-core-resource-directory-05.
- * 
+ * <p>
  * For the management of the resources, ResourceDirectory
  * uses the classes provided by the cf-rd package.
  */ 
 public class ResourceDirectory extends CoapServer{
 
+	private static ResourceDirectory resourceDirectory = null;
 	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
-
+	
 	public ResourceDirectory() {
 
 		RDResource rdResource = new RDResource(); 
@@ -28,6 +29,20 @@ public class ResourceDirectory extends CoapServer{
 		// add the rd resource to the server 
 		add(rdResource); 
 
+	}
+	
+	/**
+	 * The Resource Directory is a Singleton.
+	 * This method returns the class instance and, if it is null,
+	 * creates it before.
+	 * @return the class instance
+	 */
+	public static ResourceDirectory getInstance() {
+		if (resourceDirectory == null){
+			resourceDirectory = new ResourceDirectory();
+		}
+		
+		return resourceDirectory;
 	}
 
 	/**
@@ -44,7 +59,7 @@ public class ResourceDirectory extends CoapServer{
 		}
 	}
 
-	public static final void main(String[] args) {
+	public static void main(String[] args) {
 
 		// create resource directory
 		ResourceDirectory resourceDirectory = new ResourceDirectory();
