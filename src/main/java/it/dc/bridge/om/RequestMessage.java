c5a3992d7cbced4a2083a992284128f357505adc
David Costa <dav.costa90@gmail.com>
2016-02-22 16:20:22 +0100

    Added comments in ReqMsg and ResMsg classes

--- src/main/java/it/dc/bridge/om/RequestMessage.java
+++ src/main/java/it/dc/bridge/om/RequestMessage.java
@@ -5,48 +5,89 @@ import java.util.Map;
 import org.alljoyn.bus.annotation.Position;
 import org.alljoyn.bus.annotation.Signature;
 
+/**
+ * The Class RequestMessage is used in the AllJoyn message exchanges.
+ * It represents the CoAP request message and implements the {@link CoAPRequestMessage} interface.
+ * 
+ * @see CoAPRequestMessage
+ * @see Options
+ */
 public class RequestMessage implements CoAPRequestMessage{
 
+	/** The option fields. */
 	@Position(0)
 	@Signature("r")
 	Options opt;
 	
+	/** The query attributes */
 	@Position(1)
 	@Signature("a{ss}")
 	Map<String,String> attr;
 	
+	/** The message payload. */
 	@Position(2)
 	@Signature("ay")
 	byte[] payload;
 	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#getOptions()
+	 */
 	public Options getOptions() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#setOptions(it.dc.bridge.om.Options)
+	 */
 	public void setOptions(Options options) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#getAttributes()
+	 */
 	public Map<String, String> getAttributes() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#setAttributes(java.util.Map)
+	 */
 	public void setAttributes(Map<String, String> attributes) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#getPayload()
+	 */
 	public byte[] getPayload() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#getPayloadString()
+	 */
 	public String getPayloadString() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#setPayload(byte[])
+	 */
 	public void setPayload(byte[] payload) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPRequestMessage#setPayload(java.lang.String)
+	 */
 	public void setPayload(String payload) {
 		// TODO Auto-generated method stub
 		
--- src/main/java/it/dc/bridge/om/ResponseMessage.java
+++ src/main/java/it/dc/bridge/om/ResponseMessage.java
@@ -5,55 +5,90 @@ import org.alljoyn.bus.annotation.Signature;
 
 import it.dc.bridge.om.CoAP.ResponseCode;
 
+/**
+ * The Class ResponseMessage is used in the AllJoyn message exchanges.
+ * It represents the CoAP response message and implements the {@link CoAPResponseMessage} interface.
+ * 
+ * @see CoAPResponseMessage
+ * @see ResponseCode
+ * @see Options
+ */
 public class ResponseMessage implements CoAPResponseMessage{
 
+	/** The code. */
 	@Position(0)
 	@Signature("i")
 	ResponseCode code;
 	
+	/** The opt. */
 	@Position(1)
 	@Signature("r")
 	Options opt;
 	
+	/** The payload. */
 	@Position(2)
 	@Signature("ay")
 	byte[] payload;
 	
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#getCode()
+	 */
 	public ResponseCode getCode() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#setCode(it.dc.bridge.om.CoAP.ResponseCode)
+	 */
 	public void setCode(ResponseCode code) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#getOptions()
+	 */
 	public Options getOptions() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#setOptions(it.dc.bridge.om.Options)
+	 */
 	public void setOptions(Options options) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#getPayload()
+	 */
 	public byte[] getPayload() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#getPayloadString()
+	 */
 	public String getPayloadString() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#setPayload(byte[])
+	 */
 	public void setPayload(byte[] payload) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/* (non-Javadoc)
+	 * @see it.dc.bridge.om.CoAPResponseMessage#setPayload(java.lang.String)
+	 */
 	public void setPayload(String payload) {
 		// TODO Auto-generated method stub
 		
