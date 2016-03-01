d4fd301b66d155986025f25370f4a4b8107fdba0
David Costa <dav.costa90@gmail.com>
2016-03-01 17:39:42 +0100

    CoAP Proxy class
    Wrongly committed before

--- src/main/java/it/dc/bridge/proxy/CoAPProxy.java
+++ src/main/java/it/dc/bridge/proxy/CoAPProxy.java
@@ -1,5 +1,12 @@
 package it.dc.bridge.proxy;
 
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.Request;
+import org.eclipse.californium.core.coap.Response;
+
+import it.dc.bridge.rd.ResourceDirectory;
+
 public class CoAPProxy implements Runnable {
 
 	private static final CoAPProxy proxy = new CoAPProxy();
@@ -16,9 +23,31 @@ public class CoAPProxy implements Runnable {
 	 * 
 	 * @return the class instance
 	 */
-	public CoAPProxy getInstance() {
+	public static CoAPProxy getInstance() {
 		
 		return proxy;
+		
+	}
+	
+	public static Response callMethod(final String path, final Request request) {
+		
+		/*String node = ResourceDirectory.getInstance().getContext(null);
+		System.out.println("CoAP Server context:"+node);
+		CoapClient client = new CoapClient(node+"/.well-known/core");
+		
+		CoapResponse response = client.advanced(request);
+		if (response!=null) {
+
+			System.out.println(response.getCode());
+			System.out.println(response.getOptions());
+			System.out.println(response.getResponseText());
+
+		} else {
+			System.out.println("No response received.");
+		}*/
+		
+		Response pippo = new Response(null);
+		return pippo;
 		
 	}
 	
