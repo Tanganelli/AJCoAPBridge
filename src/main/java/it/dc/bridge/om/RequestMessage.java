2b74ae7020eed958d4ffdd2d4c6327f85f083ba8
David Costa <dav.costa90@gmail.com>
2016-03-02 10:59:14 +0100

    ReuestMessage constructors

--- src/main/java/it/dc/bridge/om/RequestMessage.java
+++ src/main/java/it/dc/bridge/om/RequestMessage.java
@@ -1,5 +1,6 @@
 package it.dc.bridge.om;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.alljoyn.bus.annotation.Position;
@@ -28,6 +29,31 @@ public class RequestMessage implements CoAPRequestMessage{
 	@Position(2)
 	@Signature("ay")
 	private byte[] payload;
+	
+	/**
+	 * Instantiates a new request message.
+	 */
+	public RequestMessage() {
+		
+		options = new Options();
+		attributes = new HashMap<String, String>();
+		payload = null;
+		
+	}
+	
+	/**
+	 * Instantiates a new option set equal to the specified message parameters.
+	 * @param options the option set
+	 * @param attributes the query attributes
+	 * @param payload the message payload
+	 */
+	public RequestMessage(Options options, Map<String,String> attributes, byte[] payload) {
+		
+		this.options = new Options(options);
+		this.setAttributes(attributes);
+		this.setPayload(payload);
+		
+	}
 	
 	/* (non-Javadoc)
 	 * @see it.dc.bridge.om.CoAPRequestMessage#getOptions()
