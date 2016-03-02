c3b5feef3547b81fa8162f99730e42f33b7f090f
David Costa <dav.costa90@gmail.com>
2016-03-02 16:27:39 +0100

    Added context map and its methods

--- src/main/java/it/dc/bridge/rd/ResourceDirectory.java
+++ src/main/java/it/dc/bridge/rd/ResourceDirectory.java
@@ -2,6 +2,8 @@ package it.dc.bridge.rd;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.network.CoapEndpoint;
@@ -9,21 +11,25 @@ import org.eclipse.californium.core.network.EndpointManager;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 
 /**
- * The class ResourceDirectory provides a RD that implements
+ * The class <tt>ResourceDirectory</tt> provides a RD that implements
  * the functions required to register, maintain and remove the
  * CoAP resources. It does not implement resource lookup, as
- * described in draft-ietf-core-resource-directory-05.
+ * described in <i>draft-ietf-core-resource-directory-05</i>.
  * <p>
- * For the management of the resources, ResourceDirectory
- * uses the classes provided by the cf-rd package.
+ * For the management of the resources, <tt>ResourceDirectory</tt>
+ * uses the classes provided by the Californium <i>cf-rd</i> package.
  */ 
 public class ResourceDirectory extends CoapServer implements Runnable {
 
 	private static final ResourceDirectory resourceDirectory = new ResourceDirectory();
 	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
 
+	/* Map containing the identifier-context pair for each registered node */
+	private Map<String,String> contexts = new HashMap<String,String>();
+	
 	/**
 	 * Instantiates a new Resource Directory.
+	 * Since it is a Singleton, the constructor is private.
 	 */
 	private ResourceDirectory() {
 
@@ -45,7 +51,7 @@ public class ResourceDirectory extends CoapServer implements Runnable {
 
 	}
 
-	/**
+	/*
 	 * Add individual endpoints listening on default CoAP port
 	 * on all IP addresses of all network interfaces.
 	 */
@@ -59,7 +65,45 @@ public class ResourceDirectory extends CoapServer implements Runnable {
 		}
 	}
 	
-
+	/**
+	 * Associates the specified context with the specified node 
+	 * in the context map. 
+	 * If the map previously contained a mapping for the node, 
+	 * the old context is replaced by the specified context.
+	 * 
+	 * @param path node identifier
+	 * @param context node context
+	 */
+	public void addContext(String path, String context) {
+		
+		contexts.put(path, context);
+		
+	}
+	
+	/**
+	 * Returns the context to which the specified node is mapped,
+	 * or null if this map contains no mapping for the node identifier
+	 * 
+	 * @param path the node identifier
+	 * @return the node context, if present
+	 */
+	public String getContext(String path) {
+		
+		return contexts.get(path);
+		
+	}
+	
+	/**
+	 * Removes the mapping for a node identifier from this map if it is present.
+	 * 
+	 * @param path the node identifier
+	 */
+	public void removeContext(String path) {
+		
+		contexts.remove(path);
+		
+	}
+	
 	public void run() {
 
 		// add endpoints on all IP addresses
