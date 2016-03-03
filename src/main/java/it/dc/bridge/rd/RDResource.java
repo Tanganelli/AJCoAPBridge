8582892ae3e64eb124fb563e4bcf3d9b71eac70a
David Costa <dav.costa90@gmail.com>
2016-03-03 12:31:48 +0100

    Set Endpoint Type during registration

--- src/main/java/it/dc/bridge/rd/RDResource.java
+++ src/main/java/it/dc/bridge/rd/RDResource.java
@@ -37,6 +37,10 @@ public class RDResource extends CoapResource {
 	/**
 	 * POSTs a new sub-resource to this resource. The name of the new 
 	 * sub-resource is a random number if not specified in the Option-query.
+	 * The registration can have a {?ep,d,et,lt,con} URI Template query.
+	 * {ep,d,et} are set here, since they are allowed only during registration.
+	 * {lt,con} are set in the <tt>setParameters</tt> function, used by the
+	 * registration update too.
 	 *
 	 * @param exchange the exchange
 	 */ 
@@ -46,17 +50,24 @@ public class RDResource extends CoapResource {
 		// get name and lifetime from option query 
 		LinkAttribute attr; 
 		String endpointIdentifier = ""; 
-		String domain = "local"; 
+		String domain = "local";
+		String endpointType = "";
+
 		RDNodeResource resource = null; 
 		List<CoapResource> resources = new ArrayList<CoapResource>();
 
 		ResponseCode responseCode; 
 
+		for(Resource n : this.getChildren()) {
+			RDNodeResource r = (RDNodeResource)n;
+			System.out.println("ep:"+r.getEndpointIdentifier()+" et:"+r.getEndpointType()+" d:"+r.getDomain()+" lt:"+r.getLifetime());
+		}
+
 		LOGGER.info("Registration request: "+exchange.getSourceAddress()); 
 
 		List<String> query = exchange.getRequestOptions().getUriQuery(); 
 		for (String q:query) { 
-			// FIXME Do not use Link attributes for URI template variables 
+
 			attr = LinkAttribute.parse(q); 
 
 			if (attr.getName().equals(LinkFormat.END_POINT)) { 
@@ -66,6 +77,11 @@ public class RDResource extends CoapResource {
 			if (attr.getName().equals(LinkFormat.DOMAIN)) { 
 				domain = attr.getValue(); 
 			} 
+
+			if (attr.getName().equals(LinkFormat.END_POINT_TYPE)) {
+				endpointType = attr.getValue();
+			}
+
 		} 
 
 		if (endpointIdentifier.equals("")) { 
@@ -87,7 +103,9 @@ public class RDResource extends CoapResource {
 				randomName = Integer.toString((int) (Math.random() * 10000)); 
 			} while (getChild(randomName) != null); 
 
-			resource = new RDNodeResource(endpointIdentifier, domain); 
+			resource = new RDNodeResource(endpointIdentifier, domain);
+			resource.setEndpointType(endpointType);
+
 			add(resource);
 
 			responseCode = ResponseCode.CREATED; 
@@ -95,7 +113,7 @@ public class RDResource extends CoapResource {
 			responseCode = ResponseCode.CHANGED; 
 		} 
 
-		// set parameters of resource 
+		// set parameters of the resource: {lt,con}
 		if (!resource.setParameters(exchange.advanced().getRequest(), resources)) { 
 			resource.delete(); 
 			exchange.respond(ResponseCode.BAD_REQUEST); 
@@ -103,20 +121,20 @@ public class RDResource extends CoapResource {
 		} 
 
 		LOGGER.info("Adding new endpoint: "+resource.getContext());
-		
+
 		String location = resource.getURI();
-		
+
 		if(!resources.isEmpty()){
 			// inform AJ Object Manager Application about the registration of a new resource
 			AJObjectManagerApp objectManager = AJObjectManagerApp.getInstance();
-			
+
 			for(CoapResource r : resources) {
 				objectManager.addResource(r.getURI());
 			}		
 		}
-		
+
 		ResourceDirectory.getInstance().addContext(resource.getEndpointIdentifier(), resource.getContext());
-		
+
 		// inform client about the location of the new resource 
 		exchange.setLocationPath(location); 
 
