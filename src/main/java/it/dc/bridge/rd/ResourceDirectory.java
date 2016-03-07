2dac04ff57811421950080606789375f0a916464
David Costa <dav.costa90@gmail.com>
2016-03-07 22:13:04 +0100

    Changed RD port for local test

--- src/main/java/it/dc/bridge/rd/ResourceDirectory.java
+++ src/main/java/it/dc/bridge/rd/ResourceDirectory.java
@@ -28,7 +28,8 @@ public class ResourceDirectory extends CoapServer implements Runnable {
 	private static final ResourceDirectory resourceDirectory = new ResourceDirectory();
 
 	/* CoAP default port */
-	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
+	// FIXME Remove -1 from CoAP port
+	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT) - 1;
 
 	/* Map containing the <identifier, context> pair for each registered node */
 	private Map<String, String> contexts = new ConcurrentHashMap<String, String>();
